8a44ad5418f02dfccfdfb50a483b7bfec9b56dda
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-08-07 15:46:45 +0100

    Github element added

--- src/Components/Socials.js
+++ src/Components/Socials.js
@@ -1,7 +1,16 @@
+import Link from 'next/link'
+import {FaGithub} from 'react-icons/fa'
+
+
+
 export default function Socials(){
     return(
         <div>
-
+            {data.socials.map((social, index)=>(
+                <Link className={`${social.title===Github?invisible:false}`} href={social.url} target="_blank" rel="noopener noreferrer" > 
+                    <FaGithub/>
+                </Link>
+            ))}
         </div>
     )
 }
